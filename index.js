ffbae9e6d64163e2b68d722056a9a4e10971595e
andrers52 <rs.andre@gmail.com>
2025-07-03 21:29:24 -0300

    fixed index.js paths

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-export { ArsCalendar } from ".components/ars-calendar/ars-calendar.js";
-export { ArsColorSelect } from ".components/ars-color-select/ars-color-select.js";
-export { ArsDialog } from ".components/ars-dialog/ars-dialog.js";
-export { ArsPageController } from ".components/ars-page/ars-page-controller.js";
-export { ArsPage } from ".components/ars-page/ars-page.js";
+export { ArsCalendar } from "./components/ars-calendar/ars-calendar.js";
+export { ArsColorSelect } from "./components/ars-color-select/ars-color-select.js";
+export { ArsDialog } from "./components/ars-dialog/ars-dialog.js";
+export { ArsPageController } from "./components/ars-page/ars-page-controller.js";
+export { ArsPage } from "./components/ars-page/ars-page.js";
 export { WebComponentBase } from "./components/web-component-base/web-component-base.js";
 // mixins
 export { LocalizedMixin } from "./mixins/localized-mixin/localized-mixin.js";
@@ -12,4 +12,3 @@ export { RemoteCallReceiverMixin } from "./mixins/remote-call-mixin/remote-call-
 export { RollMixin } from "./mixins/roll-mixin/roll-mixin.js";
 export { ShowIfPropertyTrueMixin } from "./mixins/show-if-property-true-mixin/show-if-property-true-mixin.js";
 export { SwipeableMixin } from "./mixins/swipeable-mixin/swipeable-mixin.js";
-
