c2bed0da9d8f07d135aad7e7fb7f9a4797d8397b
andre <rs.andre@gmail.com>
2026-05-27 19:30:11 -0300

    fixing drop down list mark visuals

--- src/components/ars-dialog/ars-dialog-css.ts
+++ src/components/ars-dialog/ars-dialog-css.ts
@@ -252,6 +252,8 @@ export const DEFAULT_CSS = `
   /* Select dropdown styling */
   select {
     cursor: pointer;
+    -webkit-appearance: none;
+    appearance: none;
     background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 20 20'%3e%3cpath stroke='%236b7280' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M6 8l4 4 4-4'/%3e%3c/svg%3e");
     background-position: right 8px center;
     background-repeat: no-repeat;
